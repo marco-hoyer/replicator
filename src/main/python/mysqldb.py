1255a52ef62b3c79d5423e2a74a8cfd452d88e88
mhoyer <marco_hoyer@gmx.de>
2013-07-30 20:38:38 +0200

    writes stdout of mysqldump to file

--- src/main/python/mysqldb.py
+++ src/main/python/mysqldb.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python
 
 from system import System
+from filesystem import Filesystem
 
 class MysqlDB():
 
 	def __init__(self, config):
 		if config:
 			self.system = System(config)
+			self.fs = Filesystem(config)
 			self.mysqlcmd = config["mysql_binary_path"]
 			self.mysqldumpcmd = config["mysqldump_binary_path"]
 			self.global_params = ['--defaults-extra-file=%s' % config["mysql_config_file"]]
 		else:
 			self.system = System(None)
+			self.fs = Filesystem(None)
 			self.mysqlcmd = "/usr/bin/mysql"
 			self.mysqldumpcmd = "/usr/bin/mysqldump"
 			self.global_params = ['--defaults-extra-file=/etc/mysql/debian.cnf']
 
 	def dump_database(self, dbname, target_file):
-		params = ['--single-transaction', '--databases', '--single-transaction', '--add-drop-database', dbname]
-		self.system.execute(self.mysqldumpcmd, self.global_params + params)
+		params = ['--single-transaction', '--databases', '--add-drop-database', dbname]
+		self.fs.write_file(target_file, self.system.execute(self.mysqldumpcmd, self.global_params + params))
 
 	def get_databases(self):
 		return None
